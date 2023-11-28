f6582eb5580b54db53def53919cffe01db2e9232
Laura Montana <lauvmr27@gmail.com>
2023-11-28 15:38:06 -0500

    commit prueba

---
+++ app/src/main/java/com/example/avanceproyecto/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.avanceproyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnPagar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
